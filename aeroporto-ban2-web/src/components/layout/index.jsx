77b6c2b6ac040c68e20afc735cfdcdf8104988ef
Luciano Wayand de Abreu <36968613+lucianowayand@users.noreply.github.com>
2022-11-20 17:39:54 -0300

    feat: added controlador

--- aeroporto-ban2-web/src/components/layout/index.jsx
+++ aeroporto-ban2-web/src/components/layout/index.jsx
@@ -10,9 +10,10 @@ export default function Layout({ children }) {
                 <div>
                     <h2 className="pt2 pointer" onClick={() => navigate('/')}>Airport Manager</h2>
                     <div className="pt2 flex-column">
-                        <a className="side-menu-link pointer pt2" onClick={() => navigate('/avioes')}>Aviões</a>
-                        <a className="side-menu-link pointer pt1" onClick={() => navigate('/empregados')}>Empregados</a>
+                        <a className="side-menu-link pointer pt2" onClick={() => navigate('/empregados')}>Empregados</a>
+                        <a className="side-menu-link pointer pt1" onClick={() => navigate('/controlador')}>Controlador</a>
                         <a className="side-menu-link pointer pt1" onClick={() => navigate('/modelos')}>Modelos</a>
+                        <a className="side-menu-link pointer pt1" onClick={() => navigate('/avioes')}>Aviões</a>
                         <a className="side-menu-link pointer pt1" onClick={() => navigate('/testes')}>Testes</a>
                     </div>
                 </div>
--- aeroporto-ban2-web/src/routes/index.jsx
+++ aeroporto-ban2-web/src/routes/index.jsx
@@ -2,7 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "../components/layout";
 
 import Avioes from "../pages/avioes";
+import Controlador from "../pages/controlador";
 import Empregados from "../pages/empregados";
+import Home from "../pages/home";
 import Modelos from "../pages/modelos";
 import NotFound from "../pages/not_found";
 import Testes from "../pages/testes";
@@ -12,11 +14,12 @@ export default function Router() {
         <BrowserRouter>
             <Layout>
                 <Routes>
+                    <Route path="/" element={<Home />} />
                     <Route path="/avioes" element={<Avioes />} />
                     <Route path="/empregados" element={<Empregados />} />
                     <Route path="/modelos" element={<Modelos />} />
                     <Route path="/testes" element={<Testes />} />
-
+                    <Route path="/controlador" element={<Controlador />} />
                     <Route path="*" element={<NotFound />} />
                 </Routes>
             </Layout>
